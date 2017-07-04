326b665dd8d7d31838f985b79f3b01622c6c4752
ysmu <>
2017-07-03 18:17:35 -0700

    fixed uninstall script...

--- scripts/uninstall.bat
+++ scripts/uninstall.bat
@@ -1,2 +1,9 @@
-sudo cmd /c "del %WINDIR%\SysWoW64\sudo.exe"
-sudo cmd /c "del %WINDIR%\system32\sudo.exe"
+@echo off
+
+reg query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set arch=86 || set arch=64
+
+::system32 gets redirected to SysWOW64 on 64bit machines
+sudo cmd /c "del /f %WINDIR%\system32\sudo.exe"
+if %arch%==64 (
+	sudo64 cmd /c "del /f %WINDIR%\system32\sudo.exe"
+)
