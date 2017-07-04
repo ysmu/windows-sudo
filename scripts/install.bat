5da8c80341a8bfbeaf02b3f67939144f24a56ee4
ysmu <>
2017-07-03 18:13:19 -0700

    fixed install script

--- scripts/install.bat
+++ scripts/install.bat
@@ -2,10 +2,8 @@
 
 reg query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set arch=86 || set arch=64
 
-if %arch%==86 (
-	sudo cmd /c "copy sudo.exe %WINDIR%\system32\sudo.exe"
-)
+::system32 gets redirected to SysWOW64 on 64bit machines
+sudo cmd /c "copy sudo.exe %WINDIR%\system32\sudo.exe"
 if %arch%==64 (
-	sudo cmd /c "copy sudo.exe %WINDIR%\SysWoW64\sudo.exe"
-	sudo cmd /c "copy sudo64.exe %WINDIR%\system32\sudo.exe"
+	sudo64 cmd /c "copy sudo64.exe %WINDIR%\system32\sudo.exe"
 )
